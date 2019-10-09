82ed307bff00342750b7fcb0467ef98ae3dbd39f
Emanuel Saldivia <emanuelsaldivia@gmail.com>
2019-10-09 00:19:51 -0300

    backend validation

--- api/common/models/services.js
+++ api/common/models/services.js
@@ -1,5 +1,7 @@
 'use strict';
 
 module.exports = function(Services) {
-
+    Services.validatesPresenceOf('name', 'description', 'included', 'extra-info');
+    Services.validatesUniquenessOf('name', {message: 'ya existe un servicio con este nombre'});
+    Services.validatesLengthOf('description', {max: 200, message: {max: 'Excedio el maximo de caracteres (200)'}});
 };
